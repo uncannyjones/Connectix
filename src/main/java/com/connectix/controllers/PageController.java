53fc55499b7acba1fa6cd9c748bb34208300486d
uncannyjones <ankitpujar48@gmail.com>
2024-08-24 03:25:17 +0530

    Exploring Thymleaf

--- src/main/java/com/connectix/controllers/PageController.java
+++ src/main/java/com/connectix/controllers/PageController.java
@@ -3,6 +3,9 @@ package com.connectix.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class PageController {
@@ -16,5 +19,21 @@ public class PageController {
         model.addAttribute("Name", "Connectix");
         model.addAttribute("GithubRepo","https://github.com/uncannyjones/Connectix");
         return "home";
+    }
+
+    //about
+    @RequestMapping("/about")
+    public String aboutPage(Model model){
+        model.addAttribute("isLogin", false);
+        System.out.println("About Page Loading");
+        return "about" ;
+    }
+    
+
+    //services
+    @RequestMapping("/services")
+    public String aboutServices(){
+        System.out.println("Services Page Loading");
+        return "Services" ;
     }
 }
